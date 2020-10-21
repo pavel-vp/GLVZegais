8e8b06e215bc68cdb3a20518f0b7e86326b6eae3
pavel-vp <pinegin.pavel@gmail.com>
2020-10-21 11:32:03 +0400

    Ver3. ALCO. Фото (1 версия) добавил удаление

--- app/src/main/java/com/glvz/egais/ui/doc/photo/ActPhotoList.java
+++ app/src/main/java/com/glvz/egais/ui/doc/photo/ActPhotoList.java
@@ -25,6 +25,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 import com.glvz.egais.BuildConfig;
 import com.glvz.egais.R;
@@ -66,12 +67,14 @@ public class ActPhotoList extends ActBaseDocList {
                 tryCreateNewPhoto();
             }
         });
+        registerForContextMenu(lv);
+
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        if (v.getId()==R.id.lvContent) {
+        if (v.getId()==R.id.listView) {
             MenuInflater inflater = getMenuInflater();
             inflater.inflate(R.menu.menu_photorec, menu);
         }
