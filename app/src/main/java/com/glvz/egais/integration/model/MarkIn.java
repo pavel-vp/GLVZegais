2e294a901c44dfdb31cc3db0889a7bcd55bff6c6
Pavel Pinegin <pavel.pinegin@agiloft.com>
2022-12-21 21:15:08 +0400

    Ver5. db-storage для Инв (исправление: необязательность неизветсных аттрибутов в марках)

--- app/src/main/java/com/glvz/egais/integration/model/MarkIn.java
+++ app/src/main/java/com/glvz/egais/integration/model/MarkIn.java
@@ -1,7 +1,9 @@
 package com.glvz.egais.integration.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MarkIn {
     @JsonProperty(value="Mark")
     private String mark;
