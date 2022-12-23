6b0dd5dc7523d38f750cbf84bd64d998305b6ca1
Pavel Pinegin <pavel.pinegin@agiloft.com>
2022-12-23 14:51:03 +0400

    Ver5. db-storage для Инв (исправление: индексы)

--- app/src/main/java/com/glvz/egais/daodb/AppDbHelper.java
+++ app/src/main/java/com/glvz/egais/daodb/AppDbHelper.java
@@ -19,6 +19,9 @@ public class AppDbHelper extends SQLiteOpenHelper {
                     BaseRec.KEY_DOCID + " TEXT," +
                     BaseRec.KEY_STATUS + " TEXT," +
                     BaseRec.KEY_EXPORTED + " TEXT)";
+    private static final String SQL_CREATE_INV_INDEX =
+            "CREATE INDEX "+DaoMem.KEY_INV+"_index ON " + DaoMem.KEY_INV + " ("+BaseRec.KEY_DOCID+")";
+
     private static final String SQL_CREATE_INV_CONTENT =
             "CREATE TABLE " + DaoMem.KEY_INV + DaoMem.CONTENT + " (" +
                     BaseColumns._ID + " INTEGER PRIMARY KEY," +
@@ -31,6 +34,11 @@ public class AppDbHelper extends SQLiteOpenHelper {
                     BaseRec.KEY_POS_MANUAL_MRC + " TEXT," +
                     BaseRec.KEY_POS_POSITION + " TEXT," +
                     BaseRec.KEY_POS_MARKSCANNED_CNT + " TEXT)";
+    private static final String SQL_CREATE_INV_CONTENT_INDEX_1 =
+            "CREATE INDEX "+DaoMem.KEY_INV+DaoMem.CONTENT+"_index_1 ON " + DaoMem.KEY_INV + DaoMem.CONTENT + " ("+BaseRec.KEY_DOCID+")";
+    private static final String SQL_CREATE_INV_CONTENT_INDEX_2 =
+            "CREATE INDEX "+DaoMem.KEY_INV+DaoMem.CONTENT+"_index_2 ON " + DaoMem.KEY_INV + DaoMem.CONTENT + " ("+BaseRec.KEY_DOC_CONTENTID+")";
+
     private static final String SQL_CREATE_INV_CONTENT_MARK =
             "CREATE TABLE " + DaoMem.KEY_INV + DaoMem.CONTENT_MARK +" (" +
                     BaseColumns._ID + " INTEGER PRIMARY KEY," +
@@ -41,6 +49,12 @@ public class AppDbHelper extends SQLiteOpenHelper {
                     BaseRec.KEY_POS_MARKSCANNED_ASTYPE + " TEXT," +
                     BaseRec.KEY_POS_MARKSCANNEDREAL + " TEXT," +
                     BaseRec.KEY_POS_MARKBOX + " TEXT)";
+    private static final String SQL_CREATE_INV_CONTENT_MARK_INDEX_1 =
+            "CREATE INDEX "+DaoMem.KEY_INV+DaoMem.CONTENT_MARK+"_index_1 ON " + DaoMem.KEY_INV + DaoMem.CONTENT_MARK + " ("+BaseRec.KEY_DOCID+")";
+    private static final String SQL_CREATE_INV_CONTENT_MARK_INDEX_2 =
+            "CREATE INDEX "+DaoMem.KEY_INV+DaoMem.CONTENT_MARK+"_index_2 ON " + DaoMem.KEY_INV + DaoMem.CONTENT_MARK + " ("+BaseRec.KEY_DOC_CONTENTID+")";
+    private static final String SQL_CREATE_INV_CONTENT_MARK_INDEX_3 =
+            "CREATE INDEX "+DaoMem.KEY_INV+DaoMem.CONTENT_MARK+"_index_3 ON " + DaoMem.KEY_INV + DaoMem.CONTENT_MARK + " ("+BaseRec.KEY_DOC_CONTENT_MARKID+")";
 
     private static final String SQL_DELETE_INV = "DROP TABLE IF EXISTS " + DaoMem.KEY_INV;
     private static final String SQL_DELETE_INV_CONTENT = "DROP TABLE IF EXISTS " + DaoMem.KEY_INV+DaoMem.CONTENT;
@@ -51,8 +65,16 @@ public class AppDbHelper extends SQLiteOpenHelper {
     }
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_INV);
+        db.execSQL(SQL_CREATE_INV_INDEX);
+
         db.execSQL(SQL_CREATE_INV_CONTENT);
+        db.execSQL(SQL_CREATE_INV_CONTENT_INDEX_1);
+        db.execSQL(SQL_CREATE_INV_CONTENT_INDEX_2);
+
         db.execSQL(SQL_CREATE_INV_CONTENT_MARK);
+        db.execSQL(SQL_CREATE_INV_CONTENT_MARK_INDEX_1);
+        db.execSQL(SQL_CREATE_INV_CONTENT_MARK_INDEX_2);
+        db.execSQL(SQL_CREATE_INV_CONTENT_MARK_INDEX_3);
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
